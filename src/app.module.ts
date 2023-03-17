fa155f0940916c987b024aa5d278bbffe666133e
esteban salas <esalassulca@gmail.com>
2023-03-16 21:09:55 -0500

    agregar bd mongo y crear variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './video/video.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/tiktok'), VideoModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.DATABASE),
+    VideoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
